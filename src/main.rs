9fb134a21107940f462e15a5e8030cb5dc6cc112
Devin Scholz <devinscholz1882@gmail.com>
2026-07-27 00:29:42 +0200

    Introduced Double Mats

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ fn main() {
     // the outer rings are blue and the middle 5x5 is yellow
     //
     // This was my first attempt to implement a Matt plan
-    // kept it because it is easy to cahnge
-    let input_grid: Vec<Vec<char>> = vec![
-        vec!['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-        vec!['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-        vec!['B', 'B', 'Y', 'Y', 'Y', 'Y', 'Y', 'B', 'B'],
-        vec!['B', 'B', 'Y', 'Y', 'Y', 'Y', 'Y', 'B', 'B'],
-        vec!['B', 'B', 'Y', 'Y', 'Y', 'Y', 'Y', 'B', 'B'],
-        vec!['B', 'B', 'Y', 'Y', 'Y', 'Y', 'Y', 'B', 'B'],
-        vec!['B', 'B', 'Y', 'Y', 'Y', 'Y', 'Y', 'B', 'B'],
-        vec!['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-        vec!['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+    // kept it because it is easy to change
+    let input_grid: Vec<Vec<&str>> = vec![
+        vec!["B", "B", "B", "B", "B", "B", "B", "B", "B"],
+        vec!["B", "BR", "BL", "B", "B", "B", "B", "B", "B"],
+        vec!["BD", "B", "Y", "Y", "Y", "Y", "Y", "B", "B"],
+        vec!["BU", "B", "Y", "Y", "Y", "Y", "YR", "BL", "B"],
+        vec!["B", "B", "Y", "Y", "YD", "Y", "Y", "B", "B"],
+        vec!["B", "B", "Y", "Y", "YU", "Y", "Y", "B", "B"],
+        vec!["B", "B", "Y", "Y", "Y", "YR", "YL", "B", "B"],
+        vec!["B", "B", "B", "B", "B", "B", "B", "B", "B"],
+        vec!["B", "B", "B", "B", "B", "B", "B", "B", "B"],
     ];
 
     let mut grid: Grid = Grid::new(input_grid, Some(10));
--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,11 @@
 use colored::Colorize;
-use std::fmt;
+use std::fmt::{self, format, write};
 
-// Grid represents the matt plan;
+type MatID = usize;
+
 pub struct Grid {
-    content: Vec<Vec<Cell>>,
+    cells: Vec<Vec<MatID>>,
+    mats: Vec<Mat>,
     delivery: Delivery,
     height: usize,
     width: usize,
@@ -17,13 +19,23 @@ enum Color {
 }
 
 #[derive(Clone, Debug)]
-struct Cell {
-    position: CellCoordinate,
+enum MatPostion {
+    Singe(CellCoordinate),
+    Double {
+        first: CellCoordinate,
+        second: CellCoordinate,
+    },
+}
+
+#[derive(Clone, Debug)]
+struct Mat {
+    id: MatID,
+    position: MatPostion,
     color: Color,
     owned: bool,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct CellCoordinate {
     y: usize,
     x: usize,
@@ -35,16 +47,72 @@ struct CellCoordinate {
 //          - do not be confused; this is mostly a concern for readability and display logic
 //
 struct Delivery {
-    current_load: Vec<Cell>,
-    loads: Vec<Vec<Cell>>,
+    current_load: Vec<MatID>,
+    loads: Vec<Vec<MatID>>,
     max_size: usize,
+}
+
+struct ID {
+    id: usize,
+}
+
+impl ID {
+    fn new() -> Self {
+        Self { id: 0 }
+    }
+
+    fn next(&mut self) -> usize {
+        let id = self.id;
+        self.id += 1;
+        id
+    }
+}
+
+impl MatPostion {
+    fn first(&self) -> &CellCoordinate {
+        match self {
+            MatPostion::Singe(postition) => postition,
+            MatPostion::Double { first, .. } => first,
+        }
+    }
 }
 
 impl fmt::Display for Grid {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in &self.content {
-            for cell in row {
-                write!(f, "{} ", cell)?;
+        for (y, row) in self.cells.iter().enumerate() {
+            for (x, mat_id) in row.iter().enumerate() {
+                let mat = self.get_mat_with_id(*mat_id);
+                let current = CellCoordinate::new(x, y);
+
+                let cell_text = match &mat.position {
+                    MatPostion::Singe(..) => {
+                        format!("[{}] ", mat.color)
+                    }
+
+                    MatPostion::Double { first, second } if first.y == second.y => {
+                        if &current == first {
+                            format!("[{}--", mat.color)
+                        } else if &current == second {
+                            format!("-{}] ", mat.color)
+                        } else {
+                            return Err(fmt::Error);
+                        }
+                    }
+
+                    MatPostion::Double { first, second } if first.x == second.x => {
+                        if &current == first {
+                            format!("┌{}┐ ", mat.color)
+                        } else if &current == second {
+                            format!("└{}┘ ", mat.color)
+                        } else {
+                            return Err(fmt::Error);
+                        }
+                    }
+
+                    MatPostion::Double { .. } => return Err(fmt::Error),
+                };
+
+                mat.write_mat(f, &cell_text)?;
             }
             writeln!(f)?;
         }
@@ -72,26 +140,16 @@ impl From<char> for Color {
     }
 }
 
-impl fmt::Display for Cell {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.owned {
-            false => {
-                write!(f, "[{}] ", self.color)
-            }
-            true => {
-                let s = format!("[{}] ", self.color);
-                match self.color {
-                    Color::Blue => {
-                        write!(f, "{}", s.truecolor(0, 55, 200))
-                    }
-                    Color::Yellow => {
-                        write!(f, "{}", s.truecolor(255, 255, 0))
-                    }
-                    Color::None => {
-                        write!(f, "{}", s)
-                    }
-                }
-            }
+impl Mat {
+    fn write_mat(&self, f: &mut fmt::Formatter<'_>, text: &str) -> fmt::Result {
+        if !self.owned {
+            return write!(f, "{}", text);
+        }
+
+        match self.color {
+            Color::Blue => write!(f, "{}", text.truecolor(0, 55, 200)),
+            Color::Yellow => write!(f, "{}", text.truecolor(255, 255, 0)),
+            Color::None => write!(f, "{}", text),
         }
     }
 }
@@ -99,6 +157,12 @@ impl fmt::Display for Cell {
 impl fmt::Display for CellCoordinate {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl CellCoordinate {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
     }
 }
 
@@ -111,7 +175,7 @@ impl Delivery {
         }
     }
 
-    fn add(&mut self, mut added_content: Vec<Cell>) {
+    fn add(&mut self, mut added_content: Vec<MatID>) {
         while added_content.len() + self.current_load.len() >= self.max_size {
             let remaining_space = self.max_size - self.current_load.len();
             self.current_load
@@ -126,23 +190,73 @@ impl Delivery {
 }
 
 impl Grid {
-    // common theme; creating a new method for instanciating a new struct
-    pub fn new(grid_input: Vec<Vec<char>>, max_delivery_size: Option<usize>) -> Self {
+    pub fn new(grid_input: Vec<Vec<&str>>, max_delivery_size: Option<usize>) -> Self {
+        let mut id = ID::new();
+
         let height: usize = grid_input.len();
         let width: usize = if height > 0 { grid_input[0].len() } else { 0 };
 
-        let mut grid: Vec<Vec<Cell>> = Vec::new();
+        let mut grid: Vec<Vec<MatID>> = Vec::new();
+        let mut mats: Vec<Mat> = Vec::new();
 
         for (y, row) in grid_input.iter().enumerate() {
             let mut r = Vec::new();
             for (x, item) in row.iter().enumerate() {
-                let color = Color::from(*item);
-                let cell = Cell {
-                    position: CellCoordinate { x, y },
-                    color,
-                    owned: false,
-                };
-                r.push(cell);
+                if item.len() > 2 {
+                    panic!("Faulty mat input! :c");
+                }
+
+                if item.len() == 2 {
+                    let mat_id = id.next();
+                    let color = Color::from(item.chars().nth(0).unwrap());
+
+                    let position: MatPostion = match item.chars().nth(1).unwrap() {
+                        'R' => MatPostion::Double {
+                            first: CellCoordinate::new(x, y),
+                            second: CellCoordinate::new(x + 1, y),
+                        },
+                        'D' => MatPostion::Double {
+                            first: CellCoordinate::new(x, y),
+                            second: CellCoordinate::new(x, y + 1),
+                        },
+                        'U' => {
+                            let double_id = grid[y - 1][x];
+                            r.push(double_id);
+                            continue;
+                        }
+                        'L' => {
+                            let double_id = r[x - 1];
+                            r.push(double_id);
+                            continue;
+                        }
+                        _ => {
+                            panic!("Faulty Mat Input! :c")
+                        }
+                    };
+
+                    let mat = Mat {
+                        id: mat_id,
+                        position,
+                        color,
+                        owned: false,
+                    };
+
+                    mats.push(mat);
+                    r.push(mat_id);
+                } else {
+                    let mat_id = id.next();
+                    let color = Color::from(item.chars().nth(0).unwrap());
+                    let position = MatPostion::Singe(CellCoordinate::new(x, y));
+
+                    let mat = Mat {
+                        id: mat_id,
+                        color,
+                        owned: false,
+                        position,
+                    };
+                    mats.push(mat);
+                    r.push(mat_id);
+                }
             }
             grid.push(r);
         }
@@ -150,26 +264,51 @@ impl Grid {
         let delivery = Delivery::new(max_delivery_size.unwrap_or(40));
 
         Self {
-            content: grid,
-            delivery,
+            cells: grid,
             height,
             width,
+            mats,
+            delivery,
         }
+    }
+
+    fn get_mat_with_id(&self, id: MatID) -> &Mat {
+        for mat in &self.mats {
+            if mat.id == id {
+                return mat;
+            }
+        }
+        panic!("Mat doesnt Exist!");
+    }
+
+    fn get_mut_mat_with_id(&mut self, id: MatID) -> &mut Mat {
+        for mat in &mut self.mats {
+            if mat.id == id {
+                return mat;
+            }
+        }
+        panic!("Mat doesnt Exist!");
     }
 
     pub fn build_diagonally(&mut self) {
         let (tatami_start_x, tatami_start_y) = {
-            let start = self.find_first_tatami_matt().unwrap();
+            let start = self.find_first_tatami_mat().unwrap();
             (start.x, start.y)
         };
 
-        let mut top_left_corner_area: Vec<Cell> = Vec::new();
-        for row in self.content[..tatami_start_y].iter_mut() {
-            for cell in row[..tatami_start_x].iter_mut() {
-                cell.owned = true;
-                top_left_corner_area.push(cell.clone());
-            }
+        let mut top_left_corner_area: Vec<MatID> = Vec::new();
+
+        let mat_ids: Vec<MatID> = self.cells[..tatami_start_y]
+            .iter()
+            .flat_map(|row| row[..tatami_start_x].iter().copied())
+            .collect();
+
+        for mat_id in mat_ids {
+            let mat = self.get_mut_mat_with_id(mat_id);
+            mat.owned = true;
+            top_left_corner_area.push(mat_id);
         }
+
         self.delivery.add(top_left_corner_area);
 
         println!("{}", self);
@@ -179,7 +318,7 @@ impl Grid {
 
         let mut count = 0;
 
-        while x < self.width && y < self.height {
+        while x < self.width || y < self.height {
             if x < self.width {
                 let add_cells = self.expand_right(x, tatami_start_y);
                 self.delivery.add(add_cells);
@@ -195,21 +334,17 @@ impl Grid {
             println!("{}", self);
 
             println!(
-                "Curreny Delivery Size: {}",
+                "Current Delivery Size: {}",
                 self.delivery.current_load.len()
             );
             println!("Delivery Count: {}", self.delivery.loads.len());
 
-            count += 1;
-
-            if count != 2 {
-                continue;
+            if count % 2 == 1 {
+                let v = self.expand_center(x, y);
+                self.delivery.add(v);
             }
 
-            count = 0;
-
-            let v = self.expand_center(x, y);
-            self.delivery.add(v);
+            count += 1;
         }
 
         let v = self.expand_center(self.width, self.height);
@@ -218,49 +353,57 @@ impl Grid {
         println!("{}", self);
     }
 
-    fn find_first_tatami_matt(&self) -> Option<&CellCoordinate> {
-        for cell in self.content.iter().flatten() {
-            if cell.color == Color::Yellow {
-                return Some(&cell.position);
+    fn find_first_tatami_mat(&self) -> Option<&CellCoordinate> {
+        for mat_id in self.cells.iter().flatten() {
+            let mat = self.get_mat_with_id(*mat_id);
+            if mat.color == Color::Yellow {
+                return Some(mat.position.first());
             }
         }
         None
     }
 
-    fn expand_center(&mut self, x: usize, y: usize) -> Vec<Cell> {
-        let mut v: Vec<Cell> = Vec::new();
-        for row in self.content[..y].iter_mut() {
-            for cell in row[..x].iter_mut() {
-                // if cell.owned || cell.color == Color::Blue {
-                // continue;
-                // }
-                if cell.owned {
-                    continue;
-                }
-                cell.owned = true;
-                v.push(cell.clone());
+    fn expand_center(&mut self, x: usize, y: usize) -> Vec<MatID> {
+        let mut v: Vec<MatID> = Vec::new();
+
+        let mat_ids: Vec<MatID> = self.cells[..y]
+            .iter()
+            .flat_map(|row| row[..x].iter().copied())
+            .collect();
+
+        for mat_id in mat_ids {
+            let mat = self.get_mut_mat_with_id(mat_id);
+
+            if !mat.owned {
+                mat.owned = true;
+                v.push(mat_id);
             }
         }
         v
     }
 
-    fn expand_right(&mut self, x: usize, y: usize) -> Vec<Cell> {
+    fn expand_right(&mut self, x: usize, y: usize) -> Vec<MatID> {
         let mut v = Vec::new();
-        for row in self.content[..y].iter_mut() {
-            row[x].owned = true;
-            v.push(row[x].clone());
+
+        let mat_ids: Vec<MatID> = self.cells[..y].iter().map(|row| row[x]).collect();
+        for mat_id in mat_ids {
+            let mat = self.get_mut_mat_with_id(mat_id);
+            mat.owned = true;
+            v.push(mat_id);
         }
-        // let v: Vec<Cell> = self.content[..y].iter().map(|row| row[x].clone()).collect();
+
         v
     }
 
-    fn expand_down(&mut self, x: usize, y: usize) -> Vec<Cell> {
+    fn expand_down(&mut self, x: usize, y: usize) -> Vec<MatID> {
         let mut v = Vec::new();
-        for cell in self.content[y][..x].iter_mut() {
-            cell.owned = true;
-            v.push(cell.clone());
+        let mat_ids = self.cells[y][..x].to_vec();
+
+        for mat_id in mat_ids {
+            let mat = self.get_mut_mat_with_id(mat_id);
+            mat.owned = true;
+            v.push(mat_id);
         }
-        // let v: Vec<Cell> = self.content[y][..x].to_vec();
         v
     }
 }
